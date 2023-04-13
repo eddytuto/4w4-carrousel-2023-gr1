2fad2fcbabfe2b0f41bd7c4128e732dada634b51
Eddy Martin <eddypmartin@outlook.fr>
2023-04-13 11:50:04 -0400

    s11c1 insertion des img du carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,12 +1,18 @@
 (function(){
  console.log('Début du carrousel')
 let bouton = document.querySelector(".carrousel__ouvrir")
+/* -------------------------------------------------------- Variable du carrousel */
 let carrousel  = document.querySelector(".carrousel")
 let carrousel__x = document.querySelector(".carrousel__x")
+let carrousel__figure = document.querySelector(".carrousel__figure")
+
+/* -------------------------------------------------------- Variable de la galerie */
 let galerie = document.querySelector(".galerie")
 let galerie__img = galerie.querySelectorAll("img")
 // console.log("galerie__img: " + galerie__img.length)
 // console.log(carrousel.tagName)
+
+
 /* ----------------------------------------------------  ouvrir boîte modale */
 bouton.addEventListener('mousedown', function(){
     console.log('ouvrir la boîte modale')
@@ -32,9 +38,8 @@ function ajouter_img_dans_carrousel()
     let img = document.createElement('img')
     img.setAttribute('src', elm.getAttribute('src'))
     console.log (img.getAttribute('src'))
+    carrousel__figure.appendChild(img)
   }
 }
-
-
 
 })()
